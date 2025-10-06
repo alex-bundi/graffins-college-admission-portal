a5280880acd2537948b8305da217a472497cdfd9
alex-bundi <bundialex45@gmail.com>
2025-10-06 16:22:45 +0300

    removed next btn

--- resources/js/Pages/Application/ApplicationProcessing.vue
+++ resources/js/Pages/Application/ApplicationProcessing.vue
@@ -18,37 +18,38 @@ const errors = ref({});
 const success = ref({});
 let processed = false;
 onMounted(async () => {
-    if (!processed) {
-        try {
-            // First operation
-            const bioData = await processBioData();
-            console.log('Bio data:', bioData);
+    // if (!processed) {
+    //     try {
+    //         // First operation
+    //         const bioData = await processBioData();
+    //         console.log('Bio data:', bioData);
             
-            if (bioData?.success === true) {
-                const emergencyData = await processEmergencyContacts(bioData.data.return_value);
-                console.log('Emergency data:', emergencyData);
-
-                    if(emergencyData?.success === true){
-                        const courseData = await processApplicantCourse(bioData.data.return_value);
-                         console.log('Course data:', courseData);
-
-                         if(courseData?.success === true){
-                            const applicationConversion = await processApplicationConversion(bioData.data.return_value);
-                            console.log('Conversion data:', applicationConversion);
-                            if(applicationConversion?.success === true){
-                                router.visit('/payments/amount-payable')
-
-                            }
-                            // return;
-                         }
-                    }
-            }
+    //         if (bioData?.success === true) {
             
-            processed = true;
-        } catch (error) {
-            console.error('Error in onMounted:', error);
-        }
-    }
+    //             const emergencyData = await processEmergencyContacts(bioData.data.return_value);
+    //             console.log('Emergency data:', emergencyData);
+
+    //                 if(emergencyData?.success === true){
+    //                     const courseData = await processApplicantCourse(bioData.data.return_value);
+    //                      console.log('Course data:', courseData);
+
+    //                      if(courseData?.success === true){
+    //                         const applicationConversion = await processApplicationConversion(bioData.data.return_value);
+    //                         console.log('Conversion data:', applicationConversion);
+    //                         if(applicationConversion?.success === true){
+    //                             router.visit('/payments/amount-payable')
+
+    //                         }
+    //                         // return;
+    //                      }
+    //                 }
+    //         }
+            
+    //         processed = true;
+    //     } catch (error) {
+    //         console.error('Error in onMounted:', error);
+    //     }
+    // }
 });
 
 // Simplified functions that just handle their specific logic
@@ -109,42 +110,89 @@ async function processApplicationConversion(applicantNo) {
             </div>
 
 
-            <div>
-                <ul>
-                    <li>
-                        <h2>
-                            Processing Bio Data
-                        </h2>
-                        <p>
-                            Verifying your personal information.
-                        </p>
-                    </li>
-                    <li>
-                        <h2>
-                            Processing Emergency Contacts
-                        </h2>
-                        <p>
-                            Reviewing emergency contact details.
-                        </p>
+          
+
+            <div class="m-8">
+                
+                <ol class="items-center sm:flex sm:space-x-6">
+                    <li class="relative mb-6 sm:mb-0">
+                        <div class="flex items-center">
+                            <div class="z-10 flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full ring-0 ring-white  sm:ring-8 shrink-0">
+                                <svg class="w-2.5 h-2.5 text-primaryColor" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                                    <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z"/>
+                                </svg>
+                            </div>
+                            <div class="hidden sm:flex w-full bg-gray-200 h-0.5 dark:bg-gray-700"></div>
+                        </div>
+                        <div class="mt-3 sm:pe-8">
+                            <h3 class="text-lg font-semibold text-gray-900 tracking-wider dark:text-white font-monteserat">Processing Bio Data</h3>
+                            <p class="text-base font-bold font-josefin tracking-wider text-gray-500 dark:text-gray-400">Verifying your personal information.</p>
+                        </div>
                     </li>
 
-                    <li>
-                        <h2>
-                            Processing Applicant Course
-                        </h2>
-                        <p>
-                            Finalizing your course selection.
-                        </p>
+                    <li class="relative mb-6 sm:mb-0">
+                        <div class="flex items-center">
+                            <div class="z-10 flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full ring-0 ring-white  sm:ring-8 shrink-0">
+                                <svg class="w-2.5 h-2.5 text-primaryColor" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                                    <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z"/>
+                                </svg>
+                            </div>
+                            <div class="hidden sm:flex w-full bg-gray-200 h-0.5 dark:bg-gray-700"></div>
+                        </div>
+                        <div class="mt-3 sm:pe-8">
+                            <h3 class="text-lg font-semibold text-gray-900 tracking-wider dark:text-white font-monteserat">Processing Emergency Contacts</h3>
+                            <p class="text-base font-bold font-josefin tracking-wider text-gray-500 dark:text-gray-400">Reviewing emergency contact details.</p>
+                        </div>
                     </li>
-                    <li>
-                        <h2>
-                            Converting Applicant to Student
-                        </h2>
-                        <p>
-                            Completing your registration.
-                        </p>
+
+                    <li class="relative mb-6 sm:mb-0">
+                        <div class="flex items-center">
+                            <div class="z-10 flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full ring-0 ring-white  sm:ring-8 shrink-0">
+                                <svg class="w-2.5 h-2.5 text-primaryColor" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                                    <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z"/>
+                                </svg>
+                            </div>
+                            <div class="hidden sm:flex w-full bg-gray-200 h-0.5 dark:bg-gray-700"></div>
+                        </div>
+                        <div class="mt-3 sm:pe-8">
+                            <h3 class="text-lg font-semibold text-gray-900 tracking-wider dark:text-white font-monteserat">Processing Applicant Course</h3>
+                            <p class="text-base font-bold font-josefin tracking-wider text-gray-500 dark:text-gray-400">Finalizing your course selection.</p>
+                        </div>
                     </li>
-                </ul>
+
+                    <li class="relative mb-6 sm:mb-0">
+                        <div class="flex items-center">
+                            <div class="z-10 flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full ring-0 ring-white  sm:ring-8 shrink-0">
+                                <svg class="w-2.5 h-2.5 text-primaryColor" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                                    <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z"/>
+                                </svg>
+                            </div>
+                            <div class="hidden sm:flex w-full bg-gray-200 h-0.5 dark:bg-gray-700"></div>
+                        </div>
+                        <div class="mt-3 sm:pe-8">
+                            <h3 class="text-lg font-semibold text-gray-900 tracking-wider dark:text-white font-monteserat">Converting Applicant to Student</h3>
+                            <p class="text-base font-bold font-josefin tracking-wider text-gray-500 dark:text-gray-400">Completing your registration.</p>
+                        </div>
+                    </li>
+
+                    <li class="relative mb-6 sm:mb-0">
+                        <div class="flex items-center">
+                            <div class="z-10 flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full ring-0 ring-white  sm:ring-8 shrink-0">
+                                <svg class="w-2.5 h-2.5 text-primaryColor" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                                    <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z"/>
+                                </svg>
+                            </div>
+                            <div class="hidden sm:flex w-full bg-gray-200 h-0.5 dark:bg-gray-700"></div>
+                        </div>
+                        <div class="mt-3 sm:pe-8">
+                            <h3 class="text-lg font-semibold text-gray-900 tracking-wider dark:text-white font-monteserat">Redirecting to next page</h3>
+                            <p class="text-base font-bold font-josefin tracking-wider text-gray-500 dark:text-gray-400">Registration Complete</p>
+                        </div>
+                    </li>
+
+                </ol>
+
+
             </div>
         </section>
 
